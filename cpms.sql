7c4ed3046a1d6efe8d3dc5d80633729cd54fb52d
Sk Shahnawaz <skshahnawaz2909@gmail.com>
2020-10-16 03:39:09 +0530

    sql file structured

--- cpms.sql
+++ cpms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 15, 2020 at 10:51 PM
+-- Generation Time: Oct 16, 2020 at 12:08 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -25,27 +25,116 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `lectureID` int(200) NOT NULL,
+  `studentRoll` varchar(255) NOT NULL,
+  `studentName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `lectureID`, `studentRoll`, `studentName`) VALUES
+(1, 1, '001910501044', 'Sk Shahnawaz');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `batch`
+--
+
+CREATE TABLE `batch` (
+  `id` int(200) NOT NULL,
+  `batchID` varchar(255) NOT NULL,
+  `degree` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `currentYear` int(11) NOT NULL,
+  `startYear` int(11) NOT NULL,
+  `endYear` int(11) NOT NULL,
+  `teacher` varchar(255) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `batch`
+--
+
+INSERT INTO `batch` (`id`, `batchID`, `degree`, `department`, `currentYear`, `startYear`, `endYear`, `teacher`, `timestamp`) VALUES
+(1, 'BECSE20192023', 'BE', 'CSE', 1, 2019, 2023, 'debargham15@gmail.com', '2020-10-15 21:19:09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `courseName` varchar(255) NOT NULL,
+  `courseYear` int(11) NOT NULL,
+  `courseID` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`id`, `courseName`, `courseYear`, `courseID`) VALUES
+(1, 'Data Structure and Algorithms', 2, 'DSA2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lecture`
+--
+
+CREATE TABLE `lecture` (
+  `id` int(200) NOT NULL,
+  `batchID` varchar(255) NOT NULL,
+  `startTime` varchar(255) NOT NULL,
+  `endTime` varchar(255) NOT NULL,
+  `courseID` varchar(255) NOT NULL,
+  `teacherName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `lecture`
+--
+
+INSERT INTO `lecture` (`id`, `batchID`, `startTime`, `endTime`, `courseID`, `teacherName`) VALUES
+(1, 'BECSE20192023', '16-10-2020 14:00', '16-10-2020 16:00', 'DSA2', 'debargham15@gmail.com');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `student`
 --
 
 CREATE TABLE `student` (
   `id` int(200) NOT NULL,
+  `courseID` varchar(255) NOT NULL,
   `firstName` varchar(255) NOT NULL,
   `lastName` varchar(255) NOT NULL,
   `roll` varchar(300) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `email` varchar(300) NOT NULL,
-  `password` varchar(300) NOT NULL
+  `password` varchar(300) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`id`, `firstName`, `lastName`, `roll`, `phone`, `email`, `password`) VALUES
-(1, 'Debargha', 'Mukherjee', '001910501067', '9999999999', 'debargham14@gmail.com', 'Debmukh@2206'),
-(2, 'Sk', 'Shahnawaz', '001910501044', '9999999998', 'skshahnawaz2909@gmail.com', 'abcd1234'),
-(3, 'Neeladri', 'Pal', '001910501015', '9999999999', 'neel.pal26@gmail.com', 'Neeladri@123');
+INSERT INTO `student` (`id`, `courseID`, `firstName`, `lastName`, `roll`, `phone`, `email`, `password`, `timestamp`) VALUES
+(1, 'BECSE20192023', 'Debargha', 'Mukherjee', '001910501067', '9999999999', 'debargham14@gmail.com', 'Debmukh@2206', '2020-10-15 21:37:24'),
+(2, 'BECSE20192023', 'Sk', 'Shahnawaz', '001910501044', '9999999998', 'skshahnawaz2909@gmail.com', 'abcd1234', '2020-10-15 21:37:27'),
+(3, 'BECSE20192023', 'Neeladri', 'Pal', '001910501015', '9999999999', 'neel.pal26@gmail.com', 'Neeladri@123', '2020-10-15 21:37:29');
 
 -- --------------------------------------------------------
 
@@ -59,19 +148,44 @@ CREATE TABLE `teacher` (
   `lastName` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `email` varchar(300) NOT NULL,
-  `password` varchar(300) NOT NULL
+  `password` varchar(300) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `teacher`
 --
 
-INSERT INTO `teacher` (`id`, `firstName`, `lastName`, `phone`, `email`, `password`) VALUES
-(1, 'Debargha', 'Sir', '9999999999', 'debargham15@gmail.com', 'Debmukh@2206');
+INSERT INTO `teacher` (`id`, `firstName`, `lastName`, `phone`, `email`, `password`, `timestamp`) VALUES
+(1, 'Debargha', 'Sir', '9999999999', 'debargham15@gmail.com', 'Debmukh@2206', '2020-10-15 21:06:05');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `batch`
+--
+ALTER TABLE `batch`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lecture`
+--
+ALTER TABLE `lecture`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `student`
@@ -88,6 +202,30 @@ ALTER TABLE `teacher`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `batch`
+--
+ALTER TABLE `batch`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `lecture`
+--
+ALTER TABLE `lecture`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `student`
