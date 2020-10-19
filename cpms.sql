c37ce4817c77195fd8831e68a4e52d8a9579489f
debargham14 <debargham14@gmail.com>
2020-10-19 17:34:15 +0530

    sql file changed

--- cpms.sql
+++ cpms.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 17, 2020 at 12:18 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
+-- Generation Time: Oct 19, 2020 at 02:02 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -96,10 +95,11 @@ INSERT INTO `course` (`id`, `courseName`, `courseSem`, `courseID`, `createdBy`) 
 
 CREATE TABLE `lecture` (
   `id` int(200) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `batchID` varchar(255) NOT NULL,
   `startTime` varchar(255) NOT NULL,
   `endTime` varchar(255) NOT NULL,
-  `courseID` varchar(255) NOT NULL,
+  `lectureDetails` varchar(500) NOT NULL,
   `teacherName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -107,8 +107,12 @@ CREATE TABLE `lecture` (
 -- Dumping data for table `lecture`
 --
 
-INSERT INTO `lecture` (`id`, `batchID`, `startTime`, `endTime`, `courseID`, `teacherName`) VALUES
-(1, 'BECSE20192023', '16-10-2020 14:00', '16-10-2020 16:00', 'DSA2', 'debargham15@gmail.com');
+INSERT INTO `lecture` (`id`, `email`, `batchID`, `startTime`, `endTime`, `lectureDetails`, `teacherName`) VALUES
+(1, '', 'BECSE20192023', '16-10-2020 14:00', '16-10-2020 16:00', 'DSA2', 'debargham15@gmail.com'),
+(2, 'debargham15@gmail.com', 'CSE20192020-10-20T16:30', '2020-10-20T14:30', '2020-10-20T16:30', 'Linked lists and arrays ', 'Debargha Sir'),
+(3, 'debargham15@gmail.com', 'CSE20202020-10-20T14:07', '2020-10-20T13:07', '2020-10-20T14:07', 'Object Oriented Prog', 'Debargha Sir'),
+(4, 'debargham15@gmail.com', 'CSE20202024', '2020-10-20T18:06', '2020-10-20T19:07', 'kichu ekta', 'Debargha Sir'),
+(5, 'debargham15@gmail.com', 'MECSE20202024', '2020-10-20T20:09', '2020-10-20T21:10', 'kichu ekta 2', 'Debargha Sir');
 
 -- --------------------------------------------------------
 
@@ -118,6 +122,7 @@ INSERT INTO `lecture` (`id`, `batchID`, `startTime`, `endTime`, `courseID`, `tea
 
 CREATE TABLE `notice` (
   `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `content` longtext NOT NULL,
   `batchTarget` varchar(255) NOT NULL,
   `postedBy` varchar(255) NOT NULL,
@@ -128,8 +133,9 @@ CREATE TABLE `notice` (
 -- Dumping data for table `notice`
 --
 
-INSERT INTO `notice` (`id`, `content`, `batchTarget`, `postedBy`, `createdAt`) VALUES
-(1, 'This is a demo notice !!!', 'BECSE20192023', 'Debargha Sir', '2020-10-16 16:56:36');
+INSERT INTO `notice` (`id`, `email`, `content`, `batchTarget`, `postedBy`, `createdAt`) VALUES
+(1, '', 'This is a demo notice !!!', 'BECSE20192023', 'Debargha Sir', '2020-10-16 16:56:36'),
+(2, 'debargham15@gmail.com', 'hello, i will be taking classes from tomorrow', 'BECSE20192023', 'Debargha Sir', '2020-10-18 18:33:13');
 
 -- --------------------------------------------------------
 
@@ -253,13 +259,13 @@ ALTER TABLE `course`
 -- AUTO_INCREMENT for table `lecture`
 --
 ALTER TABLE `lecture`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `notice`
 --
 ALTER TABLE `notice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `student`
